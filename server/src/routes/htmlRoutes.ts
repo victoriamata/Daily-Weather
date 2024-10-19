e14f4588a03c4de07043bffac3aef33b57a07b64
victoriamata <victoriamataxo@gmail.com>
2024-10-19 16:54:05 -0400

    Got dotenv running

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -5,10 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-console.log(__dirname);
 //logging dirname so that ts will allow me to run
+console.log(__dirname);
 // TODO: Define route to serve index.html
-
 
 
 export default router;
