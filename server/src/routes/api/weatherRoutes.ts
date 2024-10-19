d1faaf080740d2ee2ba1a799138f61059fc0fc3e
victoriamata <victoriamataxo@gmail.com>
2024-10-19 16:52:09 -0400

    Getting server running, using office hours code from Slack

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,28 @@
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
+console.log(process.env);
+console.log("Start");
+console.log(process.env.API_KEY);
+console.log(process.env.API_BASE_URL);
+console.log("end");
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', (_req: Request, res: Response) => {
+  res.status(418).send("This hasn't been implemented yet.");
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req: Request, _res: Response) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (_req: Request, _res: Response) => {});
 
 export default router;
--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -5,6 +5,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
+console.log(__dirname);
+//logging dirname so that ts will allow me to run
 // TODO: Define route to serve index.html
+
+
 
 export default router;
