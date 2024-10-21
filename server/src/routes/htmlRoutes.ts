4be897ffc7261ff392666dd6298b6b02e5e37c2c
victoriamata <victoriamataxo@gmail.com>
2024-10-20 21:47:05 -0400

    Testing app to render

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -7,16 +7,14 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 //logging dirname so that ts will allow me to run
-console.log(__dirname);
+// console.log(__dirname);
 // TODO: Define route to serve index.html
 router.use(
   express.static(path.join(__dirname, "../../../client/dist/index.html"))
 );
 
-router.get("/", (_req: Request, _res: Response) => {
-  router.use(
-    express.static(path.join(__dirname, "../../../client/dist/index.html"))
-  );
+router.get("/", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../../client/dist/index.html"));
 });
 
 export default router; // export to router
